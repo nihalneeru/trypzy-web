2c8fb878d1b2058331677aaa6d51433896ee072e
Nihal Neerukonda <nihal.neerukonda@utexas.edu>
2026-01-02 10:10:34 -0600

    resolve E2E test failure

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,9 +6,10 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    exclude: ['**/node_modules/**', '**/e2e/**', '**/.next/**'],
+    exclude: ['**/node_modules/**', '**/e2e/**', '**/.next/**', '**/dist/**'],
     watch: false, // Set to true for watch mode, but we'll use --watch flag
     setupFiles: ['./vitest.setup.ts'],
+    testTimeout: 10000,
   },
   resolve: {
     alias: {
